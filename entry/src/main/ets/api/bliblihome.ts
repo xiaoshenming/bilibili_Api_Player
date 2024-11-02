e1a7c517fa66dca7d626b4b07b4a59bb744c0508
xiaoshenming <1181584752@qq.com>
2024-11-02 17:01:00 +0800

    初步json识别完毕

---
+++ entry/src/main/ets/api/bliblihome.ts
@@ -0,0 +1,54 @@
+import http from '@ohos.net.http';
+// import { JSON } from '@kit.ArkTS';
+interface playcookie {
+  'Cookie': string;
+  'User-Agent': string;
+  'Referer': string;
+}
+export const requestSelf = async (sessdata: string, method: http.RequestMethod, extraData: any): Promise<object | null> => {
+  const url = 'https://api.bilibili.com/x/web-interface/wbi/index/top/feed/rcmd';
+  const headers: playcookie = {
+    'Cookie': `SESSDATA=${sessdata}`,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+    'Referer': 'https://www.bilibili.com/'
+  };
+
+  let httpRequest = http.createHttp();
+
+  try {
+    const response: any = await httpRequest.request(
+      url,
+      {
+        method,
+        header: headers,
+        expectDataType: http.HttpDataType.STRING,
+        usingCache: true,
+        priority: 1,
+        connectTimeout: 60000,
+        readTimeout: 60000,
+        usingProtocol: http.HttpProtocol.HTTP1_1,
+        extraData: {
+          fresh_type: 4,
+          ps: 30
+        }
+      }
+    );
+
+    if (response.responseCode === 200) {
+     // console.log('返回的信息:', JSON.stringify(response.result))
+     //  let a=JSON.parse(response.result);
+     //  console.info('返回的信息:',JSON.stringify((a as object)?.["data"]["item"][1]))
+     //  return response.result;
+      return JSON.parse(response.result);
+     //  return JSON.stringify(response.result);
+    } else {
+      console.log('请求失败:', response.responseCode);
+      return null;
+    }
+  } catch (error) {
+    console.log('请求出错:', error);
+    return null;
+  } finally {
+    httpRequest.destroy();
+  }
+}
