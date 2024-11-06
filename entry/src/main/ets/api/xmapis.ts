201598febbf8c46a2ce32fca79784b7be098ca0e
xiaoshenming <1181584752@qq.com>
2024-11-06 18:38:41 +0800

    增加私有接口网络视频完成进度80%2024年11月6日18点38分

---
+++ entry/src/main/ets/api/xmapis.ts
@@ -0,0 +1,38 @@
+import http from '@ohos.net.http';
+// import { JSON } from '@kit.ArkTS';
+interface agcookie {
+  'Accept-Language': string;
+  'User-Agent': string;
+}
+export const xmapis = async (): Promise<object | null> => {
+  // const url = 'https://api.aag.moe/api/bzspjx?url='+blibliurl;
+  const url = 'http://10.3.36.9:7894/videos';  // 发送请求到 Express 服务
+  const headers: agcookie = {
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+  };
+
+  let httpRequest = http.createHttp();
+
+  try {
+    const response: any = await httpRequest.request(url,
+      {
+        header: headers,
+        method: http.RequestMethod.GET,
+        connectTimeout: 120000,  // 设置较长的连接超时时间（例如 60 秒）
+        readTimeout: 300000     // 设置较长的读取超时时间（例如 300 秒）
+      });
+
+    if (response.responseCode === 200) {
+      return JSON.parse(response.result);
+    } else {
+      console.log('请求失败:', response.responseCode);
+      return null;
+    }
+  } catch (error) {
+    console.log('请求出错:', error);
+    return null;
+  } finally {
+    httpRequest.destroy();
+  }
+}
